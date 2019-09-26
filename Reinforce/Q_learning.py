433ff3a42d51ddeb35d1e4b3fe038c30dc1ec065
adhish9899 <aggarwaladhish@ymail.com>
2019-09-27 01:36:33 +0530

    Minor Bug Fixes

--- Reinforce/Q_learning.py
+++ Reinforce/Q_learning.py
@@ -4,6 +4,9 @@
 import gym
 import numpy as np
 
+max_num_episode = 50000
+steps_per_episode = 200 # This is specific to MountainCar. May change with env
+
 episilon_min = 0.005
 max_num_steps = max_num_episode * steps_per_episode
 episilon_decay = 500 * episilon_min / max_num_steps
@@ -11,22 +14,22 @@ alpha = 0.05 # Learning rate
 gamma = 0.98
 num_discrete_steps = 30 # Number of bins to Discretize each observation dim
 
-def q_learner(object):
+class q_learner(object):
     
     def __init__(self,env):
         self.obs_shape = env.observation_space.shape
         self.obs_high = env.observation_space.high
         self.obs_low = env.observation_space.low
-        self.obs_bins = num_discrete_steps
+        self.obs_bin = num_discrete_steps
         self.bin_width = (self.obs_high - self.obs_low)/self.obs_bin
         self.action_shape = env.action_space.n
 
         # Create a multi-dimensional array (aka. Table) to represent the Q-values
         
-        self.Q = np.zeros((self.obs_bin + 1, self.obs_bin + 1, self.action_space))
+        self.Q = np.zeros((self.obs_bin + 1, self.obs_bin + 1, self.action_shape))
         self.alpha = alpha
         self.gamma = gamma
-        self.episilon = 1.0
+        self.epsilon = 1.0
 
     
     def discretize(self,obs):
