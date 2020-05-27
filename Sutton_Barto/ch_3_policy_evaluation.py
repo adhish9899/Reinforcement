f337e9baaab1793194fa079ecd7550cacc79114d
Adhish Aggarwal <adhish@niveshi.com>
2020-05-27 23:26:21 +0530

    commit

--- Sutton_Barto/ch_3_policy_evaluation.py
+++ Sutton_Barto/ch_3_policy_evaluation.py
@@ -4,29 +4,26 @@
 import numpy as np
 import copy
 
-def action_reward(intial_position, action, reward_size):
+def action_reward(intial_position, action, terminal_states, reward_size):
 
-    if intial_position == [0,1]:
-        return [4,1], 10
-
-    if intial_position == [0,3]:
-        return [2,3], 5
-
+    if intial_position in terminal_states:
+        return intial_position, 0
+    
     final_position = np.array(intial_position) + np.array(action)
 
-    if -1 in final_position or 5 in final_position:
-        return intial_position, -1
+    if -1 in final_position or 4 in final_position:
+        final_position = intial_position
     
     return final_position, reward_size
 
 
 def gird_world():
-    gamma = 0.9
-    reward_size = 0
-    grid_size = 5
-    reward_states = [[0,1], [0,3]]
+    gamma = 1
+    reward_size = -1
+    grid_size = 4
+    termination_states = [[0,0], [grid_size-1, grid_size-1]]
     actions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
-    num_iterations = 100
+    num_iterations = 1000
 
     value_map = np.zeros((grid_size, grid_size))
     states = [[i, j] for i in range(grid_size) for j in range(grid_size)]
@@ -38,7 +35,7 @@ def gird_world():
         for state in states:
             weighted_rewards = 0
             for action in actions:
-                final_position, reward = action_reward(state, action, reward_size)
+                final_position, reward = action_reward(state, action, termination_states, reward_size)
 
                 weighted_rewards += (1/len(actions))*(reward + gamma*value_map[final_position[0], final_position[1]])
 
@@ -49,11 +46,10 @@ def gird_world():
         value_map = copy.deepcopy(copy_value_map)
         if it in [0,1,2,9, 99, num_iterations-1]:
             print("Iteration {}".format(it+1))
-            print(np.round(value_map, 1))
+            print(value_map)
             print("")
 
 
 if __name__ == "__main__":
     gird_world()
-
 
