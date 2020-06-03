e7d06534086ba8c3b88f47281d0a46a2f1116775
Adhish Aggarwal <adhish@niveshi.com>
2020-06-04 00:07:56 +0530

    on polocy monte carlo

--- Sutton_Barto/ch_5_monte_carlo.py
+++ Sutton_Barto/ch_5_monte_carlo.py
@@ -83,8 +83,163 @@ def play(policy_player, intial_state = None, inital_aciton = None):
     ## inital state of the game
     state = [usable_ace_player, player_sum, dealer_card1]
 
+    # Initialize dealer's sum
+    dealer_sum = card_value(dealer_card1) + card_value(dealer_card2)
+    usable_ace_dealer = 1 in (dealer_card1, dealer_card2)
+
+    # If the dealer sum > 21, he must hold two aces
+    if dealer_sum > 21:
+        assert dealer_sum == 22
+        # Use 1 ace as 1 instead of 11
+        dealer_sum -= 10
+    
+    assert dealer_sum <= 21
+    assert player_sum <= 21
+    
+    # game starts !!!
+    
+    # players turn
+    while True:
+        if inital_aciton is not None:
+            action = inital_aciton
+            inital_aciton = None
+        
+        else:
+            action = policy_player(usable_ace_player, player_sum, dealer_card1)
+
+        # track players trajectory for importance sampling
+        player_trajectory.append([usable_ace_player, player_sum, dealer_card1])
+
+        if action == action_stand:
+            break
+        
+        # if hit, get a new card
+        card = get_card()
+
+        # Keep track of the ace count. The usable_ace_player alone is insufficient alone as it cannot between having one ace or two
+        ace_count = int(usable_ace_player)
+        if card == 1:
+            ace_count += 1
+        
+        player_sum += card_value(card)
+
+        # If the player has a usable ace, use it as 1 to avoid busting
+        while player_sum > 21 and ace_count:
+            player_sum -= 10
+            ace_count -= 1
+        
+        # player bust
+        if player_sum > 21:
+            return state, -1, player_trajectory
+        
+        assert player_sum <= 21
+        usable_ace_player = (ace_count == 1) # As you can never use more than 1 ace as 11
+    
+    # Dealer's turn
+    while True:
+
+        # get action based on current sum
+        action = policy_dealer[dealer_sum]
+
+        if action == action_stand:
+            break
+
+        # if hit, get a new card
+        new_card = get_card()
+
+        # Keep track of the ace count. The usable_ace_player alone is insufficient alone as it cannot between having one ace or two
+        ace_count = int(usable_ace_dealer)
+        if new_card == 1:
+            ace_count += 1
+        
+        dealer_sum += card_value(new_card)
+
+        # If the dealer has a usable ace, use it as 1 to avoid busting
+        if dealer_sum > 21 and ace_count:
+            dealer_sum -= 10
+            ace_count -= 1
+        
+        # Dealer bust
+        if dealer_sum > 21:
+            return state, 1, player_trajectory
+        
+        usable_ace_dealer = (ace_count == 1)
+    
+    # Compare the sum between player and dealer
+    assert player_sum <= 21 and dealer_sum <= 21
+
+    if player_sum > dealer_sum:
+        return state, 1, player_trajectory
+    
+    elif player_sum < dealer_sum:
+        return state, -1, player_trajectory
+    
+    else:
+        return state, 0, player_trajectory
+        
+        
+def monte_carlo_on_policy(episodes):
+
+    states_usuable_aces = np.zeros((10, 10))
+
+    # Initialize counts to 1 to avoid 0 being divided
+    states_usable_ace_count = np.ones((10, 10))
+
+    states_no_usable_ace = np.zeros((10, 10))
+
+    # Initialize counts to 1 to avoid 0 being divided
+    states_no_usable_ace_count = np.ones((10, 10))
+
+    for i in range(episodes):
+        _, reward, player_trajectory = play(target_policy_play)
+
+        for (usable_ace, player_sum, dealer_card), _ in player_trajectory:
+            player_sum -= 12 # no of states
+            dealer_card -= 1
+
+            if usable_ace:
+                states_usable_ace_count[player_sum, dealer_card] += 1
+                states_usuable_aces[player_sum, dealer_card] += reward
+            
+            else:
+                states_no_usable_ace_count[player_sum, dealer_card] += 1
+                states_no_usable_ace[player_sum, dealer_card] += reward
+    
+    return states_usuable_aces/states_usable_ace_count, states_no_usable_ace/states_no_usable_ace_count
+
+
+def figure_5_1():
+
+    states_usable_ace_1, states_no_usable_ace_1 = monte_carlo_on_policy(10000)
+    states_usable_ace_2, states_no_usable_ace_2 = monte_carlo_on_policy(500000)
+
+    states = [states_usable_ace_1, states_usable_ace_2,
+              states_no_usable_ace_1, states_no_usable_ace_2]
+
+    titles = ['Usable Ace, 10000 Episodes',
+              'Usable Ace, 500000 Episodes',
+              'No Usable Ace, 10000 Episodes',
+              'No Usable Ace, 500000 Episodes']
+    
+    _, axes = plt.subplot(2, 2, figsize=(40, 30))
+    plt.subplots_adjust(wspace=0.1, hspace=0.2)
+    axes = axes.flatten()
+
+    for states, title, axis in zip(states, titles, axes):
+        fig = sns.heatmap(np.flipud(states),  cmap="YlGnBu", ax=axis, xticklabels=range(1, 11),
+                          yticklabels=list(reversed(range(12, 22)))))
+        
+        fig.set_ylabel('player sum', fontsize=30)
+        fig.set_xlabel('dealer showing', fontsize=30)
+        fig.set_title(title, fontsize=30)
+
+    plt.show()
+    # plt.savefig('../images/figure_5_1.png')
+    # plt.close()
 
     
-
+if __name__ == "__main__":
+    figure_5_1()
+    
 
 
